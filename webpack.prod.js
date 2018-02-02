cfc60e44c432883c3670556d7b98c79da829a31b
wellslogan <wellslogan@gmail.com>
2018-02-02 00:27:55 -0500

    added webpack plugin for api uri on prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -9,5 +9,8 @@ module.exports = merge(common, {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('production'),
     }),
+    new webpack.DefinePlugin({
+      'process.env.API_URI': process.env.API_URI,
+    }),
   ],
 });
